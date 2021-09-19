0fb6cf444e9aad53b1fa524ee8814687a4f76ef8
Ed Skoviak <eskoviak@gmail.com>
2021-09-19 08:44:14 -0500

    add fuel works

--- add_gas_receipt.py
+++ add_gas_receipt.py
@@ -1,3 +1,5 @@
+import json
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.sqltypes import Date
@@ -6,14 +8,17 @@ from sqlalchemy_cockroachdb import run_transaction
 from models import Expense, Expense_category, Expense_sub_category
 
 def add_fuel_receipt(session):
+    detail = {'gallons': 11.008, 'miles': 161834, 'location' : 'Red Wing, MN'}
+    
     new_expense = []
-
+    
     expense_category = session.query(Expense_category).filter(Expense_category.expense_category =='Vehicle').first()
     expense_sub_category = session.query(Expense_sub_category).filter(Expense_sub_category.expense_sub_category == 'Fuel').first()
 
     #print (expense_category.id,expense_sub_category.id)
-    new_expense.append(Expense(date='2021-09-13T22:21:00', expense_category_id=expense_category.id, expense_sub_category_id=expense_sub_category.id,
-        amount=51.79))
+    new_expense.append(Expense(date='2021-09-18T23:25:00', expense_category_id=expense_category.id,
+        expense_sub_category_id=expense_sub_category.id, amount=35.21, tender = 'mastercard swiped *4863', 
+        expense_detail = json.dumps(detail)))
     session.add_all(new_expense)   
 
 
