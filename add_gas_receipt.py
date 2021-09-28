4bc74cfe0875fa3892ad86198d3995e783395d5c
Ed Skoviak <eskoviak@gmail.com>
2021-09-28 15:29:51 -0500

    tested new add_expense

--- add_gas_receipt.py
+++ add_gas_receipt.py
@@ -11,16 +11,17 @@ from models import Expense, Expense_category, Expense_sub_category
 def add_expense(session, detail):
     new_expense = []
 
-    expense_category = session.query(Expense_category).filter(Expense_category.expense_category == detail.expense_category).first()
-    expense_sub_category = session.query(Expense_sub_category).filter(Expense_sub_category.expense_sub_category == detail.expnese_sub_category).first()
+    #print(detail)
+    expense_category = session.query(Expense_category).filter(Expense_category.expense_category == detail['expense_category']).first()
+    expense_sub_category = session.query(Expense_sub_category).filter(Expense_sub_category.expense_sub_category == detail['expense_sub_category']).first()
 
     new_expense.append(Expense(
-        date = detail.date,
+        date = detail['date'],
         expense_category_id = expense_category.id,
         expense_sub_category_id = expense_sub_category.id,
-        amount = detail.amount,
-        tender = detail.tender,
-        expense_detail = json.dumps(detail.exp_detail)
+        amount = detail['amount'],
+        tender = detail['tender'],
+        expense_detail = json.dumps(detail['exp_detail'])
     ))
 
     session.add_all(new_expense)
@@ -45,10 +46,10 @@ if __name__ == '__main__':
     detail = {
         'expense_category' : 'Misc',
         'expense_sub_category' : 'Grocery',
-        'exp_detail' : {'location' : 'Red Wing, MN', 'vendor': 'Family Fare'},
-        'date' : '2021-09-26T19:41:00Z',
-        'amount' : 9999,
-        'tender' : 'some form of tender'
+        'exp_detail' : {'location' : 'Red Wing, MN', 'vendor': 'Simple Abundance', 'Balance on Account' : 76.05},
+        'date' : '2021-09-27T20:57:26Z',
+        'amount' : 4.40,
+        'tender' : 'prepaid'
     }
     try:
         psycopg_uri = url = 'cockroachdb://ed:Kh4V3R9B7DcygecH@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/budget?sslmode=verify-full&sslrootcert=/Users/edmundlskoviak/.postgresql/ca.crt&options=--cluster%3Dgolden-dingo-2123'
