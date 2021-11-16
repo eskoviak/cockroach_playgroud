ed5c9c5ed1ce5ca2880fe2a4a4e07534ae188e65
eskoviak <eskoviak@eskoviak.com>
2021-11-15 18:52:47 -0600

    interim from odysseus

--- add_expense_cat.sql
+++ add_expense_cat.sql
@@ -1,7 +1,7 @@
 --INSERT INTO Expense_category (expense_category) VALUES ('Utility');
 
-INSERT INTO Expense_sub_category (expense_sub_category) VALUES ('Storage');
+--INSERT INTO Expense_sub_category (expense_sub_category) VALUES ('Recreation');
 
 INSERT INTO Expense_xref (expense_category_id, expense_sub_category_id)
-  VALUES ( (SELECT id FROM Expense_category WHERE expense_category = 'Misc'),
-  (SELECT id FROM Expense_sub_category WHERE expense_sub_category = 'Storage'));
+  VALUES ( (SELECT id FROM Expense_category WHERE expense_category = 'Personal'),
+  (SELECT id FROM Expense_sub_category WHERE expense_sub_category = 'Recreation'));
