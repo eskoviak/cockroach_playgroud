8040e3922bdcb958ee3a749edbc0ca7423a87440
Ed Skoviak <eskoviak@gmail.com>
2022-01-23 18:13:29 -0600

    updated

--- add_expense_cat.sql
+++ add_expense_cat.sql
@@ -1,8 +1,8 @@
 --INSERT INTO Expense_category (expense_category) VALUES ('Medical');
 
-INSERT INTO Expense_sub_category (expense_sub_category) VALUES ('Subscription');
+INSERT INTO Expense_sub_category (expense_sub_category) VALUES ('Gaming');
 
 
 INSERT INTO Expense_xref (expense_category_id, expense_sub_category_id)
   VALUES ( (SELECT id FROM Expense_category WHERE expense_category = 'Personal'),
-  (SELECT id FROM Expense_sub_category WHERE expense_sub_category = 'Subscription'));
+  (SELECT id FROM Expense_sub_category WHERE expense_sub_category = 'Gaming'));
